1a5eb6949fe13434278971c55a1f7bc4570e808a
SubZzzero <lukyanenko962@gmail.com>
2025-11-03 15:30:05 +0200

    add_pizza_card

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
 import "./Categories.css"
 import Sort from '../Sort/Sort';
+import PizzaBlock from '../PizzaBlock/PizzaBlock';
 
-export default function Categories() {
+export default function Categories(props) {
 
     const categories = ["All", "Meat", "Vegetarian", "Grill", "Spicy", "Closed"];
     const [activeCategory, setActiveCategory] = useState(0);
 
     return (
         <div className="categories">
+
             <div className="categories-inner">
+
                 <ul className="categories-list">
                     {categories.map((item, index) => (
+
                         <li
                             key={index}
                             className={`categories-item ${activeCategory === index ? 'active' : ''}`}
@@ -25,6 +29,12 @@ export default function Categories() {
                 <Sort />
 
             </div>
+            <div className="categories-headers">
+                <h3>{categories[activeCategory]}</h3>
+            </div>
+            <PizzaBlock ></PizzaBlock>
+
         </div>
+
     );
 }
---
+++ src/components/PizzaBlock/PizzaBlock.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { FiPlus } from "react-icons/fi"; // плюс
+import "./PizzaBlock.css";
+
+export default function PizzaBlock() {
+    const [activeType, setActiveType] = useState(0);
+    const [activeSize, setActiveSize] = useState(1);
+
+    const types = ["Thin", "Traditional"];
+    const sizes = ["26 cm", "30 cm", "40 cm"];
+
+    return (
+        <div className="pizza-block">
+            <h4 className="pizza-block-title">Cheeseburger Pizza</h4>
+            <img
+                className="pizza-block-image"
+                src="https://images.unsplash.com/photo-1513104890138-7c749659a591?auto=format&fit=crop&w=800&q=80"
+                alt="Pizza"
+            />
+
+            <div className="pizza-block-selector">
+                <ul className="pizza-block-list">
+                    {types.map((type, index) => (
+                        <li
+                            key={index}
+                            className={`pizza-block-item ${activeType === index ? 'active' : ''}`}
+                            onClick={() => setActiveType(index)}
+                        >
+                            {type}
+                        </li>
+                    ))}
+                </ul>
+                <ul className="pizza-block-list">
+                    {sizes.map((size, index) => (
+                        <li
+                            key={index}
+                            className={`pizza-block-item ${activeSize === index ? 'active' : ''}`}
+                            onClick={() => setActiveSize(index)}
+                        >
+                            {size}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <div className="pizza-block-bottom">
+                <div className="pizza-block-price">from $5.50</div>
+                <div className="button">
+                    <span>Add</span>
+                </div>
+            </div>
+        </div>
+    );
+}
