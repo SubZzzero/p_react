bf80351351400b3deb961b4f28c25d1721f2f314
SubZzzero <lukyanenko962@gmail.com>
2025-11-05 15:21:49 +0200

    add skeleton

--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import "./Categories.css"
 import Sort from '../Sort';
 import PizzaBlock from '../PizzaBlock';
+import Skeleton from '../PizzaBlock/Skeleton';
+import "./Categories.css"
+
 
 
 export default function Categories() {
     const URL = "https://690b168a6ad3beba00f368a7.mockapi.io/items"
     const [items, setItems] = useState([])
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         fetch(URL)
             .then(res => res.json())
             .then(data => {
                 setItems(data);
-            });
+                // WHEN PIZZA DATA LOAD FAKE PIZZA FALSE
+                setIsLoading(false);
+            })
+            .catch(() => setIsLoading(false));
     }, []);
 
 
@@ -39,7 +45,11 @@ export default function Categories() {
                 <h3>{categories[activeCategory]}</h3>
             </div>
             <div className='pizza-block-wrapper'>
-                {items.map(obj => <PizzaBlock key={obj.id} {...obj} />)}
+                {isLoading
+                    // FAKE RENDER PIZZA
+                    ? [...new Array(10)].map((_, index) => <Skeleton key={index} />)
+                    // RENDER PIZZA
+                    : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
             </div>
         </div>
 
---
+++ src/components/PizzaBlock/Skeleton.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import ContentLoader from "react-content-loader"
+
+const Skeleton = (props) => (
+    <ContentLoader
+        className="pizza-block"
+        speed={2}
+        width={300}
+        height={499}
+        viewBox="0 0 300 499"
+        backgroundColor="#ffffff"
+        foregroundColor="#f6f5f4"
+        {...props}
+    >
+        <rect x="0" y="0" rx="30" ry="30" width="300" height="499" />
+    </ContentLoader>
+)
+
+export default Skeleton;
+
